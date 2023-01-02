649db591f908d67f12060cc764e8f3c6a112f75d
David Paul <dap3065@yahoo.com>
2023-01-02 15:11:31 -0600

    Adding role seeder and traits to user class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(DogSeeder::class);
+        $this->call(RoleAndPermissionSeeder::class);
     }
 }
